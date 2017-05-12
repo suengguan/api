0ee7c63a98193e37074aab90e182e526dfefcc35
suengguan <suengguan@126.com>
2017-05-12 16:05:55 +0800

    add print msg

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	beego.Debug("this is api service")
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
